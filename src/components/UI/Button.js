eec32749a8aeec09ab177f9b93cfb5fc59e8e7da
Maher-Al-Rifai <maher.refae15@gmail.com>
2025-10-26 21:02:26 +0200

    update the projects link

--- src/components/UI/Button.js
+++ src/components/UI/Button.js
@@ -1,15 +1,46 @@
+// 
+// Button.jsx
+import React from "react";
+import { Link } from "react-router-dom";
 import classes from "./Button.module.css";
-const Button = (props) => {
+
+const Button = ({ to, link, download, children }) => {
+  // For internal navigation (React Router)
+  if (to) {
+    return (
+      <Link to={to} className={classes.button}>
+        <span>{children}</span>
+      </Link>
+    );
+  }
+
+  // For external / download links
   return (
     <a
-      href={props.link}
-      download={props.download}
-      aria-label={props.children ? props.children : "Button"}
-      rel={props.download ? undefined : "noopener noreferrer"}
+      href={link}
+      download={download}
+      target={download ? undefined : "_blank"}
+      rel={download ? undefined : "noopener noreferrer"}
       className={classes.button}
     >
-      <span>{props.children}</span>
+      <span>{children}</span>
     </a>
   );
 };
+
 export default Button;
+// import classes from "./Button.module.css";
+// const Button = (props) => {
+//   return (
+//     <a
+//       href={props.link}
+//       download={props.download}
+//       aria-label={props.children ? props.children : "Button"}
+//       rel={props.download ? undefined : "noopener noreferrer"}
+//       className={classes.button}
+//     >
+//       <span>{props.children}</span>
+//     </a>
+//   );
+// };
+// export default Button;
