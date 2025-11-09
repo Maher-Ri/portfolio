2eb87822693bdd9341ab2a3d134228a2690ad7ae
Maher-Al-Rifai <160110572+Maher-Al-Rifai@users.noreply.github.com>
2025-11-10 01:19:10 +0200

    update roadmap and cover

--- src/components/cover/Cover.js
+++ src/components/cover/Cover.js
@@ -9,26 +9,27 @@ const Cover = () => {
     window.getSelection().removeAllRanges();
   }
   return (
-    <div className={classes.cover} id="cover" onMouseOver={handleImageMouseOver}>
+    <div
+      className={classes.cover}
+      id="cover"
+      onMouseOver={handleImageMouseOver}
+    >
       <Arrows />
-      {/* <div className={classes.profile}>
+      <div className={classes.profile}>
         <div className={classes.image}>
-          <img 
-          src={myImage}
-            alt="Maher Al Rifai"
-          />
+          <img src={profile} alt="Maher Al Rifai" />
         </div>
-      </div> */}
-            <div className={classes.profile}>
-        <img src={profile} alt="Maher Al Rifai" />
       </div>
       <div className={classes.intro}>
         <div className={classes.content}>
           <h1>Maher Al Rifai</h1>
-          <p>Front-end web developer, designer and overall good human. I enjoy working with people who care on projects that make me think.</p>
-        <Button to="/projects">view my projects</Button>
+          <p>
+            Front-end web developer, designer and overall good human. I enjoy
+            working with people who care on projects that make me think.
+          </p>
+          <Button to="/projects">view my projects</Button>
         </div>
-        </div>
+      </div>
     </div>
   );
 };
